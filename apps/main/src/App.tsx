16c29ccc9ba46289289b1384bacdb71064c5845a
xwartz <stddup@gmail.com>
2024-06-28 17:39:53 +0800

    chore: optimize home page

--- apps/main/src/App.tsx
+++ apps/main/src/App.tsx
@@ -1,9 +1,29 @@
 import { ThemeProvider } from './components/theme-provider'
 import { ModeToggle } from './components/mode-toggle'
+import { Separator } from '@ui/components'
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <ModeToggle />
+      <div className="flex min-h-screen flex-col items-center justify-center">
+        <ModeToggle />
+        <div className="mb-5 flex flex-col items-center">
+          <h2 className="text-lg font-semibold mt-4">Blockchain Test DApp</h2>
+          <a href="https://github.com/xwartz" target="_blank">
+            By @xwartz
+          </a>
+        </div>
+
+        <Separator />
+
+        <div className="mt-5">
+          <h4 className="font-semibold">Test DApps:</h4>
+          <ul className="ml-6 list-disc [&>li]:mt-2">
+            <li>
+              <a href="./pages/bip322/">BIP-322</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </ThemeProvider>
   )
 }
